0b80b36b79fc722d42f93c1c181264f655434746
Руслан Аитов <r_aitov@mail.ru>
2022-09-04 10:29:44 +0300

    task 5

---
+++ task5/292.py
@@ -0,0 +1,15 @@
+def f(n):
+    step1 = f'{n:b}'
+    sum_digit = sum(map(int, step1))
+    if sum_digit % 2 == 0:
+        # 2a
+        step2 = '10' + step1[2:] + '0'
+    else:
+        # 2b
+        step2 = '11' + step1[2:] + '1'
+    return int(step2, 2)
+
+
+for i in range(35):
+    if f(i) < 35:
+        print(f(i), f'при n == {i}')
