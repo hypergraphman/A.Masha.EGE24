8fb8b3b98c09573b630d95476b63151a4ae3a6f8
r_aitov <r_aitov@mail.ru>
2024-03-13 22:37:43 +0300

    task13

--- task13/5.py
+++ task13/5.py
@@ -4,6 +4,6 @@ k = 0
 net = ip_network('10.7.46.6/255.255.240.0', False)
 for ad in net:
     t = bin(int(ad))[2:].ljust(32, '0')
-    if t[16:].count('1') >= 4:
+    if t[16:].count('1') > 4:
         k += 1
 print(k)
