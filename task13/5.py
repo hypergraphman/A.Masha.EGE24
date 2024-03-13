17acd0affcb0d1e6f89b98ebb57f0e7f34a1b151
r_aitov <r_aitov@mail.ru>
2024-03-13 22:32:58 +0300

    task13

---
+++ task13/5.py
@@ -0,0 +1,9 @@
+from ipaddress import ip_network
+
+k = 0
+net = ip_network('10.7.46.6/255.255.240.0', False)
+for ad in net:
+    t = bin(int(ad))[2:].ljust(32, '0')
+    if t[16:].count('1') >= 4:
+        k += 1
+print(k)
